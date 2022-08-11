d8e6cc8bdb2af2b523569f5e8c6232871b17cfb9
Supun <nissankasupunrasanga@gmail.com>
2022-08-11 11:11:40 +0530

    fixed config

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<bank-app>'));
-
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<bank-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+
+'/dist/<bank-app>/index.html'));});
 app.listen(process.env.PORT || 8080);
